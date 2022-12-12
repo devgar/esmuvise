7408ec4a44f98940414b8cfb447cc5cdaffbaa89
Edgar Albalate <dev.gardo@gmail.com>
2022-12-12 05:05:18 +0100

    Adds sequelize

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
+const { sequelize } = require('../models')
+
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
@@ -13,6 +15,7 @@ async function start() {
 
   const { host, port } = nuxt.options.server
 
+  await sequelize.sync()
   await nuxt.ready()
   // Build only in dev mode
   if (config.dev) {
