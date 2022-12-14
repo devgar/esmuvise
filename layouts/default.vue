101d99ad8fdf91e7c06a921a8e390d66de4a9849
Edgar Albalate <dev.gardo@gmail.com>
2022-12-14 04:54:10 +0100

    Added list of alumnos

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,19 @@
 <template>
   <v-app dark>
-    <!-- <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    -->
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>  
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn> 
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn> -->
+    <v-app-bar dense :clipped-left="clipped" fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
+
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
 
       <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Tab 1</v-tab>
-          <v-tab>Tab 2</v-tab>
-          <v-tab>Tab 3</v-tab>
-        </v-tabs>
+        <v-btn small plain nuxt to="/alumnos">Alumnos</v-btn>
+        <v-btn small plain>Asignaturas</v-btn>
+        <v-btn small plain next to="/matriculas">Matriculas</v-btn>
+        <v-btn small plain>Rubricas</v-btn>
+        <v-divider vertical />
       </template>
     </v-app-bar>
     <v-content>
@@ -55,18 +21,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
---
+++ store/index.js
@@ -0,0 +1,44 @@
+export const state = () => ({
+  alumnos: [],
+  asignaturas: [],
+  matriculas: [],
+  evaluationItems: [],
+  rubricas: [],
+  rubricaGroups: []
+})
+
+export const getters = {
+  getCounter(state) {
+    return state.counter
+  }
+}
+
+export const mutations = {
+  alumnosADD(state, payload) {
+    state.alumnos = [...state.alumnos, payload]
+  },
+  alumnosSET(state, payload) {
+    state.alumnos = payload
+  }
+}
+
+export const actions = {
+  async alumnosFETCH({ commit }) {
+    const result = await fetch('/api/alumnos')
+    const data = await result.json()
+    // eslint-disable-next-line no-console
+    console.log('GOT ::', data)
+    return commit('alumnosSET', data)
+  },
+  async alumnosPOST({ dispatch }, alumno) {
+    const opts = {
+      method: 'POST',
+      body: JSON.stringify(alumno),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }
+    await fetch('/api/alumnos', opts)
+    return dispatch('alumnosFETCH')
+  }
+}
