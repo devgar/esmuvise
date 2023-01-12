e7e8ff15e659ee10046ad023de671a875f19d19e
Edgar Albalate <dev.gardo@gmail.com>
2023-01-12 04:48:17 +0100

    Adds default layout fetch

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -8,8 +8,12 @@
 import { useAlumnoStore } from '~~/stores/alumnos'
 import { useMatriculaStore } from '~~/stores/matriculas'
 import { useAsignaturaStore } from '~~/stores/asignaturas'
+import { useRubricaGroupStore } from '~~/stores/rubricaGroups'
+import { useRubricaStore } from '~~/stores/rubricas'
 
 useAlumnoStore().fetch()
 useMatriculaStore().fetchFull()
 useAsignaturaStore().fetch()
+useRubricaGroupStore().fetch()
+useRubricaStore().fetch()
 </script>
