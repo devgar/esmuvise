e67595e00c72ffed7e6185b861959dbe16ca53ee
Edgar Albalate <dev.gardo@gmail.com>
2023-01-12 06:00:43 +0100

    fix isNaN and parseInt bug

--- frontend/server/api/asignaturas/[id]/RubricaGroup/__update.js
+++ frontend/server/api/asignaturas/[id]/RubricaGroup/__update.js
@@ -4,7 +4,7 @@ import getId from '~/server/utils/getId'
 export default defineEventHandler(async (event) => {
     const asignatura = await Asignatura.findByPk(getId(event))
     if (!asignatura) return null // TODO: return valid error
-    const { RubricaGroupId } = parseInt(await readBody(event))
-    if (isNan(RubricaGroupId)) return null // TODO: return valid error
+    const { RubricaGroupId } = await readBody(event)
+    if (isNaN(RubricaGroupId)) return null // TODO: return valid error
     return asignatura.update({ RubricaGroupId })
 })
