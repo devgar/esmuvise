0fe9fcd1d0e91c381d05a7ba37a0ea648030a3dd
Edgar Albalate <ealbalate@itbacking.com>
2023-02-23 14:06:18 +0100

    Adds Post endpoints to rubricas and rubricaGroups

---
+++ frontend/server/api/rubricas/index.post.js
@@ -0,0 +1,16 @@
+import { Rubrica } from 'database'
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+    try {
+        return await Rubrica.create(body)
+    } catch (error) {
+        if (error.name === 'SequelizeUniqueConstraintError')
+            error = createError({
+                ...error,
+                statusCode: 409,
+                statusMessage: 'CONFLICT submited unique key already exists'
+            })
+        return sendError(event, error, false)
+    }
+})
