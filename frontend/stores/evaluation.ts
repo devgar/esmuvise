7d86ffd727d1fb66b5ec809e1d3e7c2b7c9510c2
Edgar Albalate <dev.gardo@gmail.com>
2023-03-29 17:56:19 +0200

    adds id getter to evaluation store

--- frontend/stores/evaluation.ts
+++ frontend/stores/evaluation.ts
@@ -1,5 +1,6 @@
 export const useEvaluationStore = defineStore('evaluation', () => {
     const evaluation = ref(2);
-    const toggle = () => evaluation.value = (evaluation.value === 2) ? 1 : 2; 
-    return { evaluation, toggle }
+    const toggle = () => evaluation.value = (evaluation.value === 2) ? 1 : 2;
+    const id = computed(() => evaluation)
+    return { evaluation, toggle, id }
 }) 
