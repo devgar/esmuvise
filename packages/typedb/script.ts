3480bee136f03a3740d2f4c3381721b12329f3b2
Edgar Albalate <dev.gardo@gmail.com>
2023-04-17 19:39:56 +0200

    Adds prisma installation

--- packages/typedb/script.ts
+++ packages/typedb/script.ts
@@ -8,32 +8,28 @@ function genId() {
     return (Date.now() * 1000 + seq++ % 100);
 }
 
-async function main() {
-    const student = await prisma.student.create({
-        data: {
-            id: genId(),
-            firstName: "Menganito",
-            lastName: "Fulgencio",
-            dni: "12345678X",
-            roadType: "Calle",
-            address: "Castillo",
-            postalCode: "12110",
-            city: "L'Alcora",
-            telephone: "964360000",
-            birthDate: new Date(),
-            sex: "M",
-        }
-    })
+const createStudent = async (firstName: string, lastName: string) =>
+    await prisma.student.create({ data: { id: genId(), firstName, lastName }})
 
-    console.log(student)
+const createSubject = async (nameVLC: string, nameCAS: string) =>
+    await prisma.subject.create({ data: { id: genId(), nameVLC, nameCAS }})
+
+const createEnrollment = async (year: number, course: number, studentId: bigint, subjectId: bigint) =>
+    await prisma.enrollment.create({ data: { id: genId(), year, course, studentId, subjectId }})
+
+async function main() {
+    const firstName = 'Agoreto', lastName = 'Alablate', nameVLC = '123', nameCAS = '123'
+
+    // ! TRANSACTION SEEMS TO BE BROKEN, PROBLEM OF MARIADB?
+    const [student, subject] = await prisma.$transaction([
+        prisma.subject.create({ data: { id: genId(), nameVLC, nameCAS }}),
+        prisma.student.create({ data: { id: genId(), firstName, lastName }}),
+    ])
+    const enrollment = await createEnrollment(2022, 3, student.id, subject.id)
+    console.log(enrollment)
 }
 
 main()
-    .then(async () => {
+    .finally(async () => {
         await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
     })
