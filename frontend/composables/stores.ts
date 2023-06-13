6b5460027418376c73385c3902ed9c715b93c4bc
Edgar Albalate <dev.gardo@gmail.com>
2023-06-14 00:53:14 +0200

    added stores autoimports

---
+++ frontend/composables/stores.ts
@@ -0,0 +1,8 @@
+export { useAlumnoStore } from '~~/stores/alumnos'
+export { useAsignaturaStore } from '~~/stores/asignaturas'
+export { useMatriculaStore } from '~~/stores/matriculas'
+export { useEvaluationStore } from '~~/stores/evaluation'
+export { useEvaluationItemStore } from '~~/stores/evaluationItems'
+export { useRubricaStore } from '~~/stores/rubricas'
+export { useRubricaGroupStore } from '~~/stores/rubricaGroups'
+export { useEquivalenceStore } from '~~/stores/equivalences'
